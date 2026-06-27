07d8f8a640d16b04c8a29fa1127e00b2ead81e01
ViniciusSilva011 <vinicius.silva.t0114629@edu.atec.pt>
2026-06-27 23:13:13 +0100

    add cloudfront function

---
+++ uhanku.com/cloudfront/function.js
@@ -0,0 +1,13 @@
+function handler(event) {
+  let request = event.request;
+  let uri = request.uri;
+
+  // Let real files pass through: /assets/app.js, /favicon.ico, etc.
+  if (uri.includes(".")) {
+    return request;
+  }
+
+  // Send React routes like /me, /dashboard, /settings to index.html
+  request.uri = "/index.html";
+  return request;
+}
